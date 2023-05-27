b03463283d53a514270a4162945f6d150f7c2493
RusAnonym <alexandrsemin2033@gmail.com>
2023-05-27 15:33:18 +0300

    small improves

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -5,6 +5,7 @@ import { Icon56Users3Outline } from "@vkontakte/icons";
 import {
     Button,
     ButtonGroup,
+    Group,
     NavIdProps, 
     Panel,
     PanelHeader,
@@ -24,29 +25,31 @@ const MainPage: FC<NavIdProps> = ({ nav }) => {
         return (
             <Panel nav={nav}>
                 <PanelHeader separator={false} before={<HeaderLeftButtons />}>REA ACS</PanelHeader>
-                <Placeholder
-                    stretched
-                    icon={<Icon56Users3Outline />}
-                    header="Необходим аккаунт"
-                    action={
-                        <ButtonGroup mode="vertical">
-                            <Button 
-                                size="l" 
-                                stretched
-                                onClick={() => push("?modal=login-page")}
-                            >Войти в аккаунт</Button>
-                            <Button 
-                                size="l" 
-                                stretched 
-                                mode="secondary"
-                                appearance="overlay"
-                                onClick={() => Session.setPopout(<AccountNotCreated />)}
-                            >У меня нет аккаунта</Button>
-                        </ButtonGroup>
-                    }
-                >
-                Для продолжения работы необходимо авторизоваться
-                </Placeholder>
+                <Group>
+                    <Placeholder
+                        stretched
+                        icon={<Icon56Users3Outline />}
+                        header="Необходим аккаунт"
+                        action={
+                            <ButtonGroup mode="vertical">
+                                <Button 
+                                    size="l" 
+                                    stretched
+                                    onClick={() => push("?modal=login-page")}
+                                >Войти в аккаунт</Button>
+                                <Button 
+                                    size="l" 
+                                    stretched 
+                                    mode="secondary"
+                                    appearance="overlay"
+                                    onClick={() => Session.setPopout(<AccountNotCreated />)}
+                                >У меня нет аккаунта</Button>
+                            </ButtonGroup>
+                        }
+                    >
+                    Для продолжения работы необходимо авторизоваться
+                    </Placeholder>
+                </Group>
             </Panel>
         );
     }
